84edc9b830914bdb0125c8aadfb7dc8616d70766
joshpitzalis <hello@joshpitzalis.com>
2015-05-18 09:06:53 +0100

    stop watch v2

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,56 @@
-function startTimer()
-{
-	var start = new Date().getTime();
+function startTimer(timestamp){
+	clearInterval(window.tenMillisecondUpdate);
+	var start =  timestamp || new Date().getTime();
 	window.start = start;
+	window.tenMillisecondUpdate = setInterval(render, 10);
+	// $('#start').text('Reset');
 	return start;
+
+
+	
 };
+
+function addLeadingZero(n){
+	if (n<10){
+		return '0'+ n;
+	}
+	else{
+		return '' + n;
+	}
+};
+
+function render(){
+	var now = new Date().getTime();
+	var elapsed = now - window.start;
+	var mm = Math.floor(elapsed / (60 * 1000));
+	var ss =  Math.floor((elapsed - (mm * 60 * 1000))/1000);
+	var cs = Math.floor((elapsed - ((mm * 60 * 1000) + (ss * 1000)))/10);
+	var time = addLeadingZero(mm) + ':' + addLeadingZero(ss) + ':' + addLeadingZero(cs);
+	
+	$('#timer').text(time);
+
+	
+};
+
+function reset(){
+	clearInterval(window.tenMillisecondUpdate);
+	$('#timer').text('00:00:00');
+};
+
+function stop(endTime){
+
+	// need to create a variable endTime  that stores when stop is pressed.
+	
+    // elapsed = endTime - start + timeElapsed;
+    // console.log("timeElapsed: "+ timeElapsed +" ms");
+    // clearInterval(timerInterval);
+    
+    // $('#stop').hide();
+    // $('#start').show();
+    // $('#clear').show();
+    
+    // return timeElapsed;
+    };
+
+
+
--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
-test( "#timer shoudl display zeros only", function() {
-  equal( $('#timer').text(), '00:00:00', 'kaboom');
+test( "#timer should display zeros only", function() {
+	reset()
+  equal( $('#timer').text(), '00:00:00');
 });
 
 test( "#start button exist", function() {
@@ -12,12 +13,47 @@ test( "startTimer returns a timestamp", function() {
 
 test("clicking start button starts startTimer function", function(){
 	$('#start').trigger("click");
-	equal(window.start.toString().length, 13, 'Timer started');		
+	equal(window.start.toString().length, 13, 'Timer started');
+	
+
 });
 
+test("startTimer accepts optional startTime timestamp parameter", function(){
+	var timestamp = new Date().getTime() -10000;
+	startTimer(timestamp);
+	equal(window.start, timestamp, 'started the timer');
+	
+});
 
+test( 'calling render() displays #timer formatted in mm:ss:cs', function(){
+	var goBackInTime = (9 * 60 * 1000) + ( 8 * 1000) + (57 * 10)
+	// console.log(goBackInTime);
+	var timestamp = new Date().getTime() - goBackInTime;
+	startTimer(timestamp);
+	render();
+	equal($('#timer').text(), '09:08:57', 'rendered correct time');
+	reset();
+});
 
+test('the timeUpdater() updates the screen every 10 milliseconds when the start button is clicked', function(){
+	$('#start').trigger("click");
+ 	notEqual(window.start, window.tenMillisecondUpdate);
+ 	reset();
+});
 
+test('timer only starts when I click start', function(){
+	$( document ).ready();
+	equal( $('#timer').text(), '00:00:00');
+})
 
+// test( "when I click 'start' button chages to say 'reset'", function() {
+// 	$('#start').trigger("click");
+// 	equal( $('#start').text(), 'Reset');		 
+// });
+
+test( "clicking stop stops counter counting", function() {
+	$('#stop').trigger("click");
+    equal( $('#timer').text(), 'timeElapsed');
+});
 
 
