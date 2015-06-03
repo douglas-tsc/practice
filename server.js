63b19025ca8e1dd03deb077c50b9ca89e14a8eee
joshpitzalis <hello@joshpitzalis.com>
2015-06-03 09:43:01 +0100

    work in progress

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync(__dirname + '/index.html');
+var page1 = fs.readFileSync(__dirname + '/page1.html');
+
+http.createServer(function (req, res) {
+
+  var url = request.url;
+
+  if (url.length === 1) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+  }
+
+  if (url.length === 1) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(page1);
+  }
+
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
