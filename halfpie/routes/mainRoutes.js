d5ea9344209a0b9a2f1edfda63c826dc5e4b1473
Josh <joshpitzalis@gmail.com>
2015-10-12 18:54:56 +0530

    front end work on home page

--- halfpie/routes/mainRoutes.js
+++ halfpie/routes/mainRoutes.js
@@ -6,3 +6,27 @@ Router.route('/', {
     SEO.set({ title: 'Home - ' + Meteor.App.NAME });
   }
 });
+
+Router.route('/welcome', {
+  name: 'welcome',
+  action: function () {
+    this.render('welcome');
+    SEO.set({ title: 'Welcome to - ' + Meteor.App.NAME });
+  }
+});
+
+Router.route('/profile', {
+  name: 'profile',
+  action: function () {
+    this.render('profile');
+    SEO.set({ title: 'Profile - ' + Meteor.App.NAME });
+  }
+});
+
+Router.route('/people', {
+  name: 'people',
+  action: function () {
+    this.render('people');
+    SEO.set({ title: 'People - ' + Meteor.App.NAME });
+  }
+});
