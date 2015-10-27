490a7c418b46e94153826518ee3d808caf28eb66
Josh <joshpitzalis@gmail.com>
2015-10-27 14:10:50 +0530

    collections added for posts and autopublish removed

--- curocracy/client/templates/posts/posts_list.js
+++ curocracy/client/templates/posts/posts_list.js
@@ -1,13 +1,6 @@
-var postsData = [{
-    title: '"Phenomenal time" to be an entrepreneur in India: Nikesh Arora',
-    url: 'http://ecoti.ms/R1QK9a'
-}, {
-    title: 'Mitra Biotech\'s Personalization Of Cancer Care With Technology Made In India',
-    url: 'http://onforb.es/1O1ZUmP'
-}, {
-    title: 'Boosting entrepreneurship: Venture capital funds can now invest 25% in India-linked firms',
-    url: 'http://bit.ly/1OKUJY8'
-}];
+
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
---
+++ curocracy/server/fixtures.js
@@ -0,0 +1,16 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+      title: '"Phenomenal time" to be an entrepreneur in India: Nikesh Arora',
+      url: 'http://ecoti.ms/R1QK9a'
+  });
+
+  Posts.insert({
+      title: 'Mitra Biotech\'s Personalization Of Cancer Care With Technology Made In India',
+      url: 'http://onforb.es/1O1ZUmP'
+  });
+
+  Posts.insert({
+      title: 'Boosting entrepreneurship: Venture capital funds can now invest 25% in India-linked firms',
+      url: 'http://bit.ly/1OKUJY8'
+  });
+}
