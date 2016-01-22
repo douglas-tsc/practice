eeaf855d100968e3a5a1e121dc5263f9786cc454
Josh <joshpitzalis@gmail.com>
2016-01-22 12:41:42 +0530

    removed permissions tonew database fo everything to work again

--- tracker/client/components/App/NewNinja/NewNinja.jsx
+++ tracker/client/components/App/NewNinja/NewNinja.jsx
@@ -4,10 +4,23 @@ NewNinja = React.createClass({
             <div className="container">
                 <div className="row">
                     <div className="col-xs-12">
-                        <h1>New Ninja Form</h1>
+                        <h1>Add Ninja</h1>
+                        <form id="new-ninja-form" action="#">
+                            <div className="form-group">
+                                <label htmlFor="first_name">First Name:</label>
+                                <input type="text" id="first_name" name="first_name" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="last_name">Last Name:</label>
+                                <input type="text" id="last_name" name="last_name" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-primary">Add Ninja</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 });
