da7f2145ff15a7039fa6ae7e2331f92ec1906bb4
Josh <joshpitzalis@gmail.com>
2016-01-23 11:33:48 +0530

    login and register errors handled

--- tracker/client/components/App/Auth/Login/Login.jsx
+++ tracker/client/components/App/Auth/Login/Login.jsx
@@ -1,31 +1,39 @@
 Login = React.createClass({
-  login(e) {
-    e.preventDefault();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    Meteor.loginWithPassword(email, password, FlowRouter.go('/'));
-  },
-  render() {
-    return (
-      <div className="col-xs-6">
-        <h1>Login to NinjaTracker</h1>
-        <form onSubmit={this.login} id="login-form" acti
-on="#">
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email"
-className="form-control"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="p
+    login(e) {
+        e.preventDefault();
+        var email = $('#email').val();
+        var password = $('#password').val();
+        Meteor.loginWithPassword(email, password, function(error) {
+            if (error) {
+                sAlert.error(error.reason, {
+                    effect: 'genie',
+                    position: 'top'
+                });
+            } else {
+                FlowRouter.go('/');
+            }
+        });
+    },
+    render() {
+        return (
+            <div className="col-xs-6">
+                <h1>Login to NinjaTracker</h1>
+                <form onSubmit={this.login} id="login-form" acti on="#">
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="p
 assword" className="form-control"/>
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-pri
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-pri
 mary">Login</button>
-          </div>
-        </form>
-      </div>
-); }
+                    </div>
+                </form>
+            </div>
+        );
+    }
 });
