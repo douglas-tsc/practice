5bb6c5174d5716b2367640f9e1f2fb17e0fd0056
Josh <joshpitzalis@gmail.com>
2016-02-02 13:24:15 +0530

    now you can register

--- finder/client/components/App/Nav/Nav.jsx
+++ finder/client/components/App/Nav/Nav.jsx
@@ -1,4 +1,26 @@
 Nav = React.createClass({
+    mixins: [ReactMeteorData],
+   getMeteorData() {
+       return {
+           loggedIn: !!Meteor.user()
+       };
+   },
+   logout() {
+       Meteor.logout(function(err) {
+           FlowRouter.go('/login');
+       });
+   },
+   getLoginLink() {
+       if (this.data.loggedIn) {
+           return <li>
+               <a onClick={this.logout} href="#">Logout</a>
+           </li>;
+       } else {
+           return <li>
+               <a href="/login">Login</a>
+           </li>;
+       }
+   },
     render() {
         return (
             <ul className="nav nav-pills nav-stacked">
