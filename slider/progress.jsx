2ac362db4444baed96044af77763d2162e8a234a
Josh <joshpitzalis@gmail.com>
2016-02-12 20:17:54 +0530

    Added state to <Progress /> component

--- slider/progress.jsx
+++ slider/progress.jsx
@@ -1,17 +1,24 @@
 /*jshint esversion: 6 */
 
 Progress = React.createClass({
-  render() {
-    return <div className="react-progress-example">
-      <input
-        ref="range"
-        type="range"
-        min="0"
-        max="100"
-        defaultValue={ this.state.value }
-        onChange={ this.setValue }
-      />
-      <ProgressBar current={ this.state.value } total="100" />
-    </div>;
-  }
+    getInitialState() {
+        return {value: 25};
+    },
+    setValue() {
+        this.setState({
+            value: this.refs.range.getDOMNode().value
+        });
+    },
+    render() {
+        return <div className="react-progress-example">
+            <input
+                ref="range"
+                type="range"
+                min="0"
+                max="100"
+                defaultValue={this.state.value}
+                onChange={this.setValue}/>
+            <ProgressBar current={this.state.value} total="100"/>
+        </div>;
+    }
 });
