975add4c308deac0628b3399781d578c7803229b
Josh <joshpitzalis@gmail.com>
2016-02-16 16:58:16 +0530

    basic meteor react page setup

---
+++ randomReviews/app.jsx
@@ -0,0 +1,53 @@
+
+App = React.createClass({
+  getTasks() {
+    return [
+      { _id: 1, text: "This is task 1" },
+      { _id: 2, text: "This is task 2" },
+      { _id: 3, text: "This is task 3" }
+    ];
+  },
+
+  renderTasks() {
+    return this.getTasks().map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
+
+
+Task = React.createClass({
+  propTypes: {
+    // This component gets the task to display through a React prop.
+    // We can use propTypes to indicate it is required
+    task: React.PropTypes.object.isRequired
+  },
+  render() {
+    return (
+      <li>{this.props.task.text}</li>
+    );
+  }
+});
+
+if (Meteor.isClient) {
+  // This code is executed on the client only
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}
