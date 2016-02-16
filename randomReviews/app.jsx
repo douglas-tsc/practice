b1d9746e147d4dc1d4bf5d261611325b49b333ec
Josh <joshpitzalis@gmail.com>
2016-02-16 17:25:02 +0530

    added goodreads apit component but not working yet

--- randomReviews/app.jsx
+++ randomReviews/app.jsx
@@ -1,15 +1,15 @@
 
 App = React.createClass({
-  getTasks() {
+  getReviews() {
     return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
+      { _id: 1, text: "This is review 1" },
+      { _id: 2, text: "This is review 2" },
+      { _id: 3, text: "This is review 3" }
     ];
   },
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.getReviews().map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
@@ -18,12 +18,14 @@ App = React.createClass({
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Review List</h1>
         </header>
 
         <ul>
           {this.renderTasks()}
         </ul>
+
+        <goodreads/>
       </div>
     );
   }
