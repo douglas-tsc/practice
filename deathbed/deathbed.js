fae9853b861573098360221003468decbb0d5274
Josh <joshpitzalis@gmail.com>
2016-02-18 18:16:44 +0530

    collection and text input added

--- deathbed/deathbed.js
+++ deathbed/deathbed.js
@@ -1,35 +1,74 @@
-if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault('counter', 0);
+Memory = new Mongo.Collection("memories");
 
-    Template.hello.helpers({
+if (Meteor.isClient) {
+    Session.setDefault('counter', 0);
+    Session.setDefault('peeps', false);
+    Session.setDefault('work', false);
+    Session.setDefault('alone', false);
+
+    Template.points.helpers({
         counter: function() {
             return Session.get('counter');
         }
     });
 
-    Template.hello.events({
+    Template.points.events({
 
         'click #work': function(event, instance) {
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('work', "WORK");
             $(event.currentTarget).prop('disabled', true);
         },
         'click #people': function(event, instance) {
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('peeps', "PEOPLE");
             $(event.currentTarget).prop('disabled', true);
         },
         'click #alone': function(event, instance) {
             Session.set('counter', Session.get('counter') + 1);
+            Session.set('alone', 'ALONE');
                 $(event.currentTarget).prop('disabled', true);
-            }
-            //
-            // 'click button': function() {
-            //     // increment the counter when button is clicked
-            //     Session.set('counter', Session.get('counter') + 1);
-            //     $(this).prop('disabled');
-            // }
+
+            },
+            'click #none': function(event, instance) {
+                // Session.set('counter', Session.get('counter') + 1);
+                    $(event.currentTarget).prop('disabled', true);
+                }
 
     });
+
+
+  Template.body.helpers({
+      tasks: function () {
+        return Memory.find({});
+      }
+  });
+
+
+    Template.notes.events({
+    "submit .new-note": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+
+      // Insert a task into the collection
+      Memory.insert({
+          count: Session.get('counter'),
+          people: Session.get('peeps'),
+          work: Session.get('work'),
+          alone: Session.get('alone'),
+        note: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
