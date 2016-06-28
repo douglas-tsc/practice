1b409ad9122000e4ce8a5effefd8772a872755ab
Josh <joshpitzalis@gmail.com>
2016-06-28 12:30:58 +0800

    images filtered and rendering

---
+++ client/components/imageDetail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ImageDetail = (props) => {
+  return (
+    <li className='media list-group-item'>
+      <div className='media-left'>
+        <img src={props.image.link} />
+      </div>
+      <div className='media-body'>
+        <h4 classname='media-heading'>
+          {props.image.title}
+        </h4>
+        <p>{props.image.description}</p>
+      </div>
+    </li>
+  );
+};
+
+export default ImageDetail;
--- client/main.js
+++ client/main.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import ImageList from './components/imageList';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      React App #2
-    </div>
-  );
-};
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {images: []};
+  }
+
+  componentWillMount () {
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0').then(response => this.setState({images: response.data.data}));
+  }
+
+  render () {
+    return (
+      <div>
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
+}
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
