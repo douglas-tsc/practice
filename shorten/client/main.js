4c300fab5e48ab8c6e25da470df1be1db4819180
Josh <joshpitzalis@gmail.com>
2016-07-06 23:06:22 +0530

    meteor method in place

--- shorten/client/main.js
+++ shorten/client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import LinkCreate from './components/linkCreate';
-
+import { Links } from '../imports/collections/links';
 const App = () => {
   return (
     <div>
--- shorten/imports/collections/links.js
+++ shorten/imports/collections/links.js
@@ -1,3 +1,9 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+  'links.insert': function (url) {
+    console.log('saving', url);
+  }
+});
+
 export const Links = new Mongo.Collection('links');
