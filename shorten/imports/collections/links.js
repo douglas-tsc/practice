0bdd70e39d5a3b557342cecfa30d78501251d9b1
Josh <joshpitzalis@gmail.com>
2016-07-09 21:27:13 +0530

    link now validates

--- shorten/imports/collections/links.js
+++ shorten/imports/collections/links.js
@@ -1,8 +1,11 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
   'links.insert': function (url) {
-    console.log('saving', url);
+    validUrl.isUri(url);
+    check(url, Match.Where(url => validUrl.isUri(url)));
   }
 });
 
