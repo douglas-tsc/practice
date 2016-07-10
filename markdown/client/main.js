c2a9dead047531ec0348cb3fe5078a1c750ae586
Josh <joshpitzalis@gmail.com>
2016-07-10 16:06:52 +0530

    react router and routes added

--- markdown/client/main.js
+++ markdown/client/main.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
+import { Router, Route, IndexRouter, browserHistory } from 'react-router';
+import BinsMain from './components/bins/binsMain';
+import BinsList from './components/bins/binsList';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <Route path='bins' component={BinsMain}></Route>
+    </Route>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.app'));
+  ReactDOM.render(routes, document.querySelector('.app'));
 });
