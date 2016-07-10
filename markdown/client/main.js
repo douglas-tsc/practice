ec98fd718afa78ce154c6de963018e91e6d0e4b8
Josh <joshpitzalis@gmail.com>
2016-07-10 16:39:54 +0530

    markdown editor added to bin pages

--- markdown/client/main.js
+++ markdown/client/main.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
-import { Router, Route, IndexRouter, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import BinsMain from './components/bins/binsMain';
 import BinsList from './components/bins/binsList';
 
 const routes = (
   <Router history={browserHistory}>
     <Route path='/' component={App}>
-      <Route path='bins' component={BinsMain}></Route>
+      <IndexRoute component={BinsList} />
+      <Route path='bins/:binId' component={BinsMain} />
     </Route>
   </Router>
 );
