5369b1aee6f4fbc7f7d46f442115dddefc72b062
Josh <joshpitzalis@gmail.com>
2016-07-10 13:57:24 +0530

    now you can create, store and render new bins

--- markdown/client/components/app.js
+++ markdown/client/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header';
+import BinsList from './bins/binsList';
 
 export default () => {
   return (
     <div>
       <Header />
+      <BinsList />
     </div>
   );
 };
