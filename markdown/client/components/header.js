075092c428602acfd11932148c58323dbd86f7d3
Josh <joshpitzalis@gmail.com>
2016-07-10 13:33:45 +0530

    blaze authentication added

--- markdown/client/components/header.js
+++ markdown/client/components/header.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import Accounts from './accounts';
 
 class Header extends Component {
   render () {
     return (
-      <nav classname='nav navbar-default'>
+      <nav className='nav navbar-default'>
         <div className='navbar-header'>
           <a className='navbar-brand'>Markbin</a>
         </div>
         <ul className='nav navbar-nav'>
           <li>
-            <a>Sign Up</a>
+            <Accounts />
           </li>
           <li>
             <a>Create Bin</a>
