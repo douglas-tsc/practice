d9c3846aaf5cda50bc5f9e55f64a7b38eaf177f3
Josh <joshpitzalis@gmail.com>
2016-11-29 13:00:12 +0530

    now you can see your order

--- react/catch-of-the-day/src/components/app.js
+++ react/catch-of-the-day/src/components/app.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -33,6 +34,15 @@ class App extends React.Component {
     });
   }
 
+  addToOrder (key) {
+    // for each key grab teh state of order and copy it
+    const order = {...this.state.order};
+    // update or create for that key's fish
+    order[key] = order[key] + 1 || 1;
+    // set the new state
+    this.setState({order});
+  }
+
   render () {
     return (
       <div className='catch-of-the-day'>
@@ -41,10 +51,14 @@ class App extends React.Component {
           <ul className='list-of-fishes'>
             {Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+              .map(key => <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />)}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     );
