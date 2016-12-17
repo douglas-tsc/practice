4df4407d97380787831c2e1a887df225c5789dcc
Josh <joshpitzalis@gmail.com>
2016-12-17 21:43:27 +0530

    added basic news client

---
+++ react/news/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+
+class App extends Component {
+
+  constructor (props) {
+    super (props);
+    this.state = {
+      list,
+      query: ''
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  render() {
+    const { query, list } = this.state;
+
+    return (
+      <div className="page">
+        <div className="interactions">
+        <Search value={query} onChange={this.onSearchChange}>
+        Search</Search>
+        </div>
+        <Table list={list} pattern={query} />
+      </div>
+    );
+  }
+}
+
+const Search = ({ value, onChange, children }) => <form> {children} <input type='text' value={value} onChange={onChange} /></form>;
+
+const Table = ({ list, pattern }) => <div className='table'> {list.filter(isSearched(pattern)).map((item) => <div key={item.objectID} className='table-row'>
+    <span><a href={item.url}>{item.title}</a></span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </div>)}
+</div>;
+
+
+export default App;
