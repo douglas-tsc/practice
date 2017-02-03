95c43bf0bdaabedea89f589e18425c2217703743
Josh <joshpitzalis@gmail.com>
2017-02-03 11:43:45 +0530

    added log

--- node/expressworks/ex8.js
+++ node/expressworks/ex8.js
@@ -3,10 +3,10 @@ const app = express();
 var fs = require('fs');
 
 app.get('/books', function (req, res) {
-  fs.readFile(process.argv[3], function (err, data){
+  fs.readFile(process.argv[3], function (err, data) {
     if (err) throw err;
     res.json(JSON.parse(data));
-  }) 
+  });
 });
 
 app.listen(process.argv[2]);
