22c2dfe8262f0a7ed31cdb1a88d9d57e4cb049dc
Josh <joshpitzalis@gmail.com>
2017-02-15 11:51:06 +0530

    added auth app to node

--- node/ninja/app.js
+++ node/ninja/app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var tweets = require('./routes/tweets');
+var login = require('./routes/login');
+
+const passport = require('passport');
+const session = require('express-session');
 
 var app = express();
 
@@ -20,6 +23,9 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret: 'i love dogs', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
@@ -32,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/tweets', tweets);
-
+app.use('/login', login);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
