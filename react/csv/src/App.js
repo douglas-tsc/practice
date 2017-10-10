60cf47b4db494a2514e2d37a56aa538f59a9b71d
Josh <joshpitzalis@gmail.com>
2017-10-10 14:44:32 +0530

    updated firstore conversion  app

--- react/csv/src/App.js
+++ react/csv/src/App.js
@@ -1,57 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { csv } from './source';
-import { db } from './firebase';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { csv } from './source'
+import { db } from './firebase'
 
 class App extends Component {
-  componentDidMount() {
+  componentDidMount () {
     csv.map(item => {
-      db
+      const data = {
+        Date: item.Date,
+        create_date: item.create_date,
+        account_id: item.account_id,
+        cookie_id: item.cookie_id,
+        UUID: item.UUID,
+        JumpshotPixel: item['Jumpshot pixel'],
+        segment: item.segment,
+        campaign_name: item.campaign_name,
+        advertiser_id: item.advertiser_id,
+        campaign_id: item.campaign_id,
+        sequential_id: item.sequential_id,
+        ip_address: item.ip_address,
+        device_id: item.device_id,
+        Time_Zone: item.Time_Zone,
+        OS: item.OS,
+        operating_system: item.operating_system,
+        creative: item.creative,
+        page_title: item.page_title,
+        URL: item.URL,
+        Clean_Domain: item.Clean_Domain,
+        impression: item.impression,
+        click: item.click
+      }
+      return db
         .collection('users')
         .doc(item.UUID)
-        .set({
-          Date: item.Date,
-          create_date: item.create_date,
-          account_id: item.account_id,
-          cookie_id: item.cookie_id,
-          UUID: item.UUID,
-          JumpshotPixel: item['Jumpshot pixel'],
-          segment: item.segment,
-          campaign_name: item.campaign_name,
-          advertiser_id: item.advertiser_id,
-          campaign_id: item.campaign_id,
-          sequential_id: item.sequential_id,
-          ip_address: item.ip_address,
-          device_id: item.device_id,
-          Time_Zone: item.Time_Zone,
-          OS: item.OS,
-          operating_system: item.operating_system,
-          creative: item.creative,
-          page_title: item.page_title,
-          URL: item.URL,
-          Clean_Domain: item.Clean_Domain,
-          impression: item.impression,
-          click: item.click
+        .collection('records')
+        .add(data)
+        .catch(function (error) {
+          console.error('Error adding document: ', error)
         })
-        .catch(function(error) {
-          console.error('Error adding document: ', error);
-        });
-    });
+    })
   }
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
