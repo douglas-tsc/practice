3072e9cd3d377d8251fb98a4c95e58ee3839719a
Josh <joshpitzalis@gmail.com>
2017-11-05 14:32:49 +0530

    added firebase connect

--- react/firebase/src/firebase.js
+++ react/firebase/src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 require('firebase/firestore')
 
-const config = {
+var config = {
   apiKey: 'AIzaSyBLm-M-hjnS9x3piyS0kER1fHk-29eptB8',
   authDomain: 'frontend-2ba5a.firebaseapp.com',
   databaseURL: 'https://frontend-2ba5a.firebaseio.com',
